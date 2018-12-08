1d88990bcdf93f8e5fd4e420763424e6bf1470cc
Captain Caius <241342+captaincaius@users.noreply.github.com>
2018-12-08 15:10:08 +0200

    Added a test ensuring tests don't die on encountering a ziggeo recorder

--- e2e/page-objects/home.po.ts
+++ e2e/page-objects/home.po.ts
@@ -1,11 +1,15 @@
 import { browser, by, element } from 'protractor';
 
-export class AppPage {
+export class HomePage {
   navigateTo() {
     return browser.get('/');
   }
 
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  getPlayerLink() {
+    return element(by.css('.navbar-nav > li:first-child a'));
   }
 }
